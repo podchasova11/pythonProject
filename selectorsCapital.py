c155c80e087ca4a3fa94d621bd1d0c4b02f6db8a
Mila Podchasova <Pod4asova20@gmail.com>
2023-07-18 23:27:35 +0300

    Update selectorsCapital.py

--- selectorsCapital.py
+++ selectorsCapital.py
@@ -2,8 +2,8 @@
 
 class MenuUS11TradingStrategiesGuide:
     
-    SUB_MENU_EN_TRADING_STRATEGIES_GUIDE = (By.CSS_SELECTOR,
-                                            "div .grid > a[href='https://capital.com/trading-strategies-guide']")
+    # SUB_MENU_EN_TRADING_STRATEGIES_GUIDE = (By.CSS_SELECTOR,
+    #                                         "div .grid > a[href='https://capital.com/trading-strategies-guide']")
     SUB_MENU_AR_TRADING_STRATEGIES_GUIDE = (By.CSS_SELECTOR,
                                             "div .grid > a[href='https://capital.com/ar/trading-strategies-guide']")
     SUB_MENU_BG_TRADING_STRATEGIES_GUIDE = (By.CSS_SELECTOR,
