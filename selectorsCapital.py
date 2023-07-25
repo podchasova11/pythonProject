290c2101b012fb835167bb58463ac4e2b11c578a
Mila Podchasova <Pod4asova20@gmail.com>
2023-07-25 10:15:59 +0300

    Update selectorsCapital.py

--- selectorsCapital.py
+++ selectorsCapital.py
@@ -275,6 +275,8 @@ BUTTON_START_TRADING_IN_ARTICLE     находится в C:\Users\Mila\PycharmP
 ###  2 LOCATORS Button [START TRADING]: Вложенных страниц                                             
 нашел AnVirus: в проекте Capital
 ul > li:nth-child(1) > a.js_signup
+
+
 нашла те же локаторы я:
 1) #c42 > ul.list-row.js-analyticsVisible > li:nth-child(1) > a
 2) ul.list-row >li:nth-child(1) > a
