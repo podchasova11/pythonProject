1c3aedcbd00649a137982e2fbe86b70e7fc3fcda
Mila Podchasova <Pod4asova20@gmail.com>
2023-08-04 12:53:49 +0300

    Update US_11-03-08-01_trading_psychology_guide_test.py

--- US_11-03-08-01_trading_psychology_guide_test.py
+++ US_11-03-08-01_trading_psychology_guide_test.py
@@ -19,26 +19,33 @@ def pytest_generate_tests(metafunc):
     """
     if "cur_item_link" in metafunc.fixturenames:
         name_file = "tests/US_11_Education/US_11-03-08_Trading_Psychology_Guide/list_of_href.txt"
-        
+
         list_item_link = list()
         try:
-            with open(name_file, "r") as file:
-                for line in file:
-                    list_item_link.append(line[:-1])
+            file = open(name_file, "r")
         except FileNotFoundError:
             print(f"{datetime.now()}   There is no file with name {name_file}!")
+        else:
+            for line in file:
+                list_item_link.append(line[:-1])
+            file.close()
 
         if len(list_item_link) == 0:
-            pytest.exit("Отсутствуют тестовые данные: нет списка ссылок на страницы")
+            pytest.skip("Отсутствуют тестовые данные: отсутствует список ссылок на страницы")
 
         metafunc.parametrize("cur_item_link", list_item_link, scope="class")
 
 
-@pytest.mark.us_11_03_08
+@pytest.fixture()
+def cur_time():
+    """Fixture"""
+    return str(datetime.now())
+
+
 class TestTradingPsychologyGuideItem:
     page_conditions = None
 
-    @allure.step("Start test of button [Start trading] on Main banner")
+    @allure.step("Start test_11.03.08_01 Click button [Start trading] on Main banner")
     def test_01_main_banner_start_trading_button(
             self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_item_link,
             prob_run_tc):
@@ -51,6 +58,9 @@ class TestTradingPsychologyGuideItem:
                              "11.03.08", "Educations > Menu item [Trading Psychology Guide]",
                              "01", "Testing button [Start Trading] on Main banner")
 
+        if cur_language not in [""]:
+            pytest.skip(f"Test-case not for '{cur_language}' language")
+
         page_conditions = Conditions(d, "")
         page_conditions.preconditions(
             d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
@@ -58,19 +68,16 @@ class TestTradingPsychologyGuideItem:
         test_element = MainBannerStartTrading(d, cur_item_link)
         test_element.arrange_(d, cur_item_link)
 
-        if not test_element.element_click():
-            pytest.fail("Testing element is not clicked")
+        test_element.element_click()
 
         test_element = AssertClass(d, cur_item_link)
         match cur_role:
-            case "NoReg":
+            case "NoReg" | "Reg/NoAuth":
                 test_element.assert_signup(d, cur_language, cur_item_link)
-            case "Reg/NoAuth":
-                test_element.assert_login(d, cur_language, cur_item_link)
             case "Auth":
-                test_element.assert_trading_platform_v2(d, cur_item_link)
+                test_element.assert_trading_platform_v2(d, cur_item_link, True)
 
-    @allure.step("Start test of button [Try demo] on Main banner")
+    @allure.step("Start test_11.03.08_02 Click button [Try demo] on Main banner")
     def test_02_main_banner_try_demo_button(
             self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_item_link,
             prob_run_tc):
@@ -83,6 +90,9 @@ class TestTradingPsychologyGuideItem:
                              "11.03.08", "Educations > Menu item [Trading Psychology Guide]",
                              "02", "Testing button [Try demo] on Main banner")
 
+        if cur_language not in [""]:
+            pytest.skip(f"Test-case not for '{cur_language}' language")
+
         page_conditions = Conditions(d, "")
         page_conditions.preconditions(
             d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
@@ -90,19 +100,16 @@ class TestTradingPsychologyGuideItem:
         test_element = MainBannerTryDemo(d, cur_item_link)
         test_element.arrange_(d, cur_item_link)
 
-        if not test_element.element_click():
-            pytest.fail("Testing element is not clicked")
+        test_element.element_click()
 
         test_element = AssertClass(d, cur_item_link)
         match cur_role:
-            case "NoReg":
+            case "NoReg" | "Reg/NoAuth":
                 test_element.assert_signup(d, cur_language, cur_item_link)
-            case "Reg/NoAuth":
-                test_element.assert_login(d, cur_language, cur_item_link)
             case "Auth":
-                test_element.assert_trading_platform_v2(d, cur_item_link, demo=True)
+                test_element.assert_trading_platform_v2(d, cur_item_link, True)
 
-    @allure.step("Start test of buttons [Trade] in Widget Most traded block")
+    @allure.step("Start test_11.03.08_03 Click buttons [Trade] in Widget Most traded block")
     def test_03_most_traded_trade_button(
             self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_item_link,
             prob_run_tc):
