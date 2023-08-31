742dc010f3246af873882110d624acd4c3b0797f
podchasova11 <Pod4asova20@gmail.com>
2023-08-31 14:29:38 +0300

    commit

---
+++ new.py
@@ -0,0 +1,8 @@
+
+
+#  Создайте функцию get_days, которая принимает две даты и возвращает количество дней между ними.
+
+import datetime
+
+def get_days(date1, date2):
+    return (date2 - date1).days
