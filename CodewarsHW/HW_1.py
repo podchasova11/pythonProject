657170b82804f6b0c25065a941b989d69104be51
podchasova11 <Pod4asova20@gmail.com>
2023-09-06 11:44:06 +0300

    add new files

---
+++ CodewarsHW/HW_1.py
@@ -0,0 +1,18 @@
+#  HW_1
+#  Create a method that takes as input a name,
+#  city, and state to welcome a person.
+#  Note that name will be an array consisting of one
+#  or more values that should be joined together with
+#  one space between each, and the length of the name
+#  array in test cases will vary.
+#
+# Example:
+#
+# ['John', 'Smith'], 'Phoenix', 'Arizona'
+# This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+print("Hello, ", ['John', 'Smith'], "! Welcome to ", "Phoenix, Arizona!" )
+name = input(str(" name:"))
+name2 = input(str("name2:"))
+print("Hello, ", name, name2, "! Welcome to ", "Phoenix, Arizona!")
+
