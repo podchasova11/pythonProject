95ecb2ab496ea46473ca7cbbb61d46425134ac1b
podchasova11 <Pod4asova20@gmail.com>
2023-09-16 13:12:47 +0300

    hw_5 added

---
+++ HW_Py/HW_5.py
@@ -0,0 +1,45 @@
+class Human:
+    def __init__(self, gender, age, weight, height, eyes_color):
+        self.gender = gender
+        self.__age = age
+        self.weight = weight
+        self.height = height
+        self.eyes_color = eyes_color
+
+    def move(self):
+        print('I can walk and run')
+
+    def get_age(self):
+        print(self.__age)
+
+    def set_age(self, new_age):
+        self.__age = new_age
+
+    def super_mother(self):
+        print('I can feed my family')
+
+    def super_father(self):
+        print('I can help my family')
+
+
+person1 = Human('f', 36, 54, 154, 'brown')
+person1.move()
+print(person1.get_age())
+print(person1.__dict__)
+print(person1._Human__age)
+person1.set_age(18)
+print(person1.__dict__)
+person1.weight = 45
+person1._Human__age = 21
+print(person1.__dict__)
+
+person_mother = Human('f', 36, 51, 154, 'brown')
+person_mother.move()
+print(person_mother.get_age())
+print(person1.__dict__)
+print(person_mother._Human__age)
+person_mother.set_age(18)
+print(person1.__dict__)
+person1.weight = 45
+person1._Human__age = 21
+print(person1.__dict__)
