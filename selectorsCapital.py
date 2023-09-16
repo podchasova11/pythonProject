fc2b8d520aa5775b10ac92173774d78484ca2183
Mila Podchasova <Pod4asova20@gmail.com>
2023-09-17 01:47:01 +0300

    Update selectorsCapital.py

--- selectorsCapital.py
+++ selectorsCapital.py
@@ -323,3 +323,42 @@ EDUCATION -> Trading_psychology_guide_locators.py
                 test_element.assert_trading_platform_v2(d, link)
 
 
+
+# это похожий тест в моем исполнении:
+    @allure.step("Start test of button [1. Create & verify your account] in Block 'Steps trading'")
+    def test_09_create_and_verify_your_account_button_in_block_steps_trading(
+            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, prob_run_tc):
+        """
+        Check: Button [1. Create & verify your account] in block 'Steps trading'
+        Language: En. License: All.
+        """
+        print(f"\n{datetime.now()}   Работает obj {self} с именем TC_11.03.03_09")
+        build_dynamic_arg_v2(self, d, worker_id, cur_language, cur_country, cur_role, prob_run_tc,
+                             "11.03.03", "Education > Menu item [Trend Trading]",
+                             "09", "Testing button [1. Create & verify your account] in Block 'Steps trading'")
+
+        if cur_language != "":
+            pytest.skip("This test-case only for english language")
+
+        page_conditions = Conditions(d, "")
+        link = page_conditions.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
+
+        page_menu = MenuSection(d, link)
+        page_menu.menu_education_move_focus(d, cur_language)
+        link = page_menu.sub_menu_trend_trading_move_focus_click(d, cur_language)
+
+        test_element = BlockStepTrading(d, link)
+        test_element.arrange_(d, link)
+
+        if not test_element.element_click():
+            pytest.fail("Testing element is not clicked")
+
+        test_element = AssertClass(d, link)
+        match cur_role:
+            case "NoReg" | "Reg/NoAuth":
+                test_element.assert_signup(d, cur_language, link)
+            case "Auth":
+                test_element.assert_trading_platform_v2(d, link)
+
+
