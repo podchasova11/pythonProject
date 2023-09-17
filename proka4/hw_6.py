2a5693353c4f072624d2f151fd8f697a2fc31992
podchasova11 <Pod4asova20@gmail.com>
2023-09-17 23:57:25 +0300

    new

--- proka4/hw_6.py
+++ proka4/hw_6.py
@@ -8,23 +8,56 @@
 #   - Возраст студента (ключ: "age")
 #   - Список предметов (type list), которые он изучает (ключ: "subjects")
 #   - Средний балл (ключ: "average_score")
+from urllib import response
+
 student = {
-    "name" : "Mila",
-    "age" : 21,
-    "subjects" : ["M", "A", "D"],
-    "average_score" : "hjjfjfjf"
+    "name": "Mila",
+    "age": 20,
+    "subjects": ["M", "A", "D"],
+    "average_score": 10
     }
-type_list = student
-print(type_list["subjects"])
-print(student)
-
-
-
+type_list = ["M", "A", "D"]
+print(type_list[0])
+print(type_list)
 
 # 3. Выведите на экран значение каждого ключа из словаря `student` с помощью доступа к элементам по ключу.
+keys = student.keys()
+values = student.values()
+print(keys)
 # 4. Обновите значение ключа "average_score" в словаре `student` на новое значение (например, 4.75).
+student["average_score"] = 4.75
+print(student)
 # 5. Добавьте новый предмет в список предметов студента (например, "Физика").
+print(student["subjects"])
+print(student)
+sub = "Fizics"
+type_list.append(sub)
+print(type_list)
+print(keys)
+print(values)
+for keys, values in student.items():
+    print(f"Ключ:{keys}, Значение: {values}")
 # 6. Удалите ключ "age" из словаря `student`, так как эта информация больше не нужна.
-# 7. Проверьте наличие ключей "age" и "gender" в словаре `student`. Выведите соответствующее сообщение, если ключ существует или не существует.
+print(student.pop("age"))
+print(student)
+for keys, values in student.items():
+    print(f"Ключ:{keys}, Значение: {values}")
+    print(student.keys())
+# 7. Проверьте наличие ключей "age" и "gender" в словаре `student`.
+# Выведите соответствующее сообщение, если ключ существует или не существует.
+print(student.keys())
+print(values)
+
+if "name" in student:
+    print("Ключ 'name' существует в словаре")
+assert "age" in student.keys(), "Элемент 'age' отсутствует в ответе"
+
+assert "gender" in student.keys(), "Элемент 'gender' отсутствует в ответе"
+print("Element is present")
+
 # 8. Получите список всех ключей словаря `student` и выведите его на экран.
+print(student.keys())
+
 # 9. Получите список всех значений словаря `student` и выведите его на экран.
+
+print(student.values)
