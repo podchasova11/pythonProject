d049404d8b9ced592e72534de155c29c5c3ca3db
podchasova11 <Pod4asova20@gmail.com>
2023-10-13 13:37:42 +0300

    new

--- proka4/hw_7.py
+++ proka4/hw_7.py
@@ -1,5 +1,15 @@
 # Домашнее задание  7
 # Домашнее задание на тему "Циклы"
+n = 0
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
+
 
 # Create a sample collection
 users = {'Hans': 'active',
