cec80bfc96cf8e21e94686ee3efb7b0962f5af26
podchasova11 <Pod4asova20@gmail.com>
2023-11-02 13:47:37 +0300

    new

---
+++ proka4/python_pytest_test/HW_2/click_tutor.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://demoqa.com/login")
