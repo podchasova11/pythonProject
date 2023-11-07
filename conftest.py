38f9a04d33f89cb01fc05efb97274b6b58ee36b8
podchasova11 <Pod4asova20@gmail.com>
2023-11-07 13:33:44 +0300

    new

--- conftest.py
+++ conftest.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
+import pytest
+from webdriver_manager.chrome import ChromeDriverManager
 
+
+@pytest.fixture(scope='function')
 def driver():
-    driver = webdriver.Chrome
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
