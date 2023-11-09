a42a10d3cad380e293783557e852c8d83a5faeac
podchasova11 <Pod4asova20@gmail.com>
2023-11-09 04:43:41 +0300

    added

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,11 +1,14 @@
 import random
-from pages.elements_page import TextBoxPage
+
+from pages.base_page import BasePage
 
 
-class TestTextBox:
+
+class TestTextBox():
 
     # ('Check TextBox')
     def test_text_box(self, driver):
+        self.driver = driver
         text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
         text_box_page.open()
         full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
