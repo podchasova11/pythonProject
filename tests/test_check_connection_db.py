fd4e7a920fa4a7850dac5a8b40b5299575f44893
podchasova11 <Pod4asova20@gmail.com>
2023-12-21 23:42:59 +0300

    new

--- tests/test_check_connection_db.py
+++ tests/test_check_connection_db.py
@@ -4,4 +4,15 @@ from proka4.pytest.less_5 import connect_data_base
 class TestDB:
 
     @pytest.mark.db
-    def test_connection_is_success(self, connect_database)
+    @pytest.mark.usefixtures("generate_data_way_2")
+    def test_login_2(self):
+        print(self.login)
+        print(self.password)
+
+    @pytest.mark.db
+    def test_connection_is_success(self, connect_database):
+        cursor = connect_database.cursor()
+        cursor.execute("SELECT * FROM employess")
+        print(cursor.fetchall())
+        connect_database.close
+
