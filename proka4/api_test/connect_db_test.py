77cc8749279cdba9b212f850c6960c3796cc0da4
podchasova11 <Pod4asova20@gmail.com>
2023-12-23 20:01:56 +0300

    new

---
+++ proka4/api_test/connect_db_test.py
@@ -0,0 +1,24 @@
+import sqlite3
+import pytest
+import requests
+
+
+@pytest.fixture()
+def connect_data_base():
+    connection = sqlite3.connect("test.db")
+    print("Мы подключили БД")
+    return connection
+
+cursor = connect_data_base().cursor()
+cursor.execute("SELECT *FROM employees")
+cursor.fetchall()  # собери все записи и запринть
+connect_data_base().close()   #закрывает БД
+
+def connect_database2():
+    connect2 = sqlite3.connect("test2.db")
+    return connect2
+
+cursor2 = connect_database2().cursor()
+cursor2.execute("SELECT * FROM employees")
+print(cursor2.fetchall())
+connect_database2()close()
