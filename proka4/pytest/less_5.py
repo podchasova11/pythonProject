19bc18a6a89a832118d06de4e2074da6bb46a3ac
podchasova11 <Pod4asova20@gmail.com>
2023-12-23 10:06:40 +0300

    new

--- proka4/pytest/less_5.py
+++ proka4/pytest/less_5.py
@@ -11,3 +11,11 @@ cursor.execute("SELECT *FROM employees")
 cursor.fetchall()  # собери все записи и запринть
 connect_data_base().close()   #закрывает БД
 
+def connect_database2():
+    connect2 = sqlite3.connect("test2.db")
+    return connect2
+
+cursor2 = connect_database2().cursor()
+cursor2.execute("SELECT * FROM employees")
+print(cursor2.fetchall())
+connect_database2()close()
