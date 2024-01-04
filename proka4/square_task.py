042ec380ad49d83e7326fbb4017e2f4166725822
podchasova11 <Pod4asova20@gmail.com>
2024-01-04 10:52:43 +0300

    new task function square

---
+++ proka4/square_task.py
@@ -0,0 +1,11 @@
+# Написать функцию, которая принимает список,
+# и возвращает этот же список, где все числа возведены в квадрат
+
+def square(numbers):
+    for number in range(len(numbers)):
+        numbers[number] **= 2
+    print(numbers)
+
+    numbers_list = [1, 2, 3, 4, 5]
+    square(numbers_list)
+    print(numbers_list)
