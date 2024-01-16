4448866188a5ba862a2d8fc39b9b7aa2b667f4e9
Mila Podchasova <Pod4asova20@gmail.com>
2024-01-16 20:54:06 +0300

    Update HW_2.py

--- CodewarsHW/HW_2.py
+++ CodewarsHW/HW_2.py
@@ -1,12 +1,3 @@
-#  20
-#  Your classmates asked you to copy some paperwork for them.
-#  You know that there are 'n' classmates and the paperwork has 'm' pages.
-#
-# Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
-#
-# Example:
-# n= 5, m=5: 25
-# n=-5, m=5:  0
 
 def paperwork(n, m):
     if m > 0 and n >0:
@@ -87,4 +78,15 @@ def cookie1(x):
 
 def main(verb, noun):
     return str(verb, noun)
+
+
+#  20
+#  Your classmates asked you to copy some paperwork for them.
+#  You know that there are 'n' classmates and the paperwork has 'm' pages.
+#
+# Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+#
+# Example:
+# n= 5, m=5: 25
+# n=-5, m=5:  0
 
