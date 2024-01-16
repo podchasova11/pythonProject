05b53c4d0fc16dea3987b144fc9ba4082dbf9968
Mila Podchasova <Pod4asova20@gmail.com>
2024-01-16 20:53:37 +0300

    Update HW_1.py

--- CodewarsHW/HW_1.py
+++ CodewarsHW/HW_1.py
@@ -13,11 +13,6 @@
 # name = ['John', 'Smith']
 # print(f"Hello, {name[0]} {name[1]}! Welcome to Phoenix, Arizona!")
 
-#  № 2 Jenny has written a function that returns a greeting for a user.
-#  However, she's in love with Johnny, and would like to greet him slightly different.
-#  She added a special case to her function, but she made a mistake.
-#
-# Can you help her?
 
 import codewars_test as test
 from solution import say_hello
@@ -31,5 +26,11 @@ def fixed_tests():
         test.assert_equals(say_hello(['Wallace','Russel','Osbourne'],'Albany','New York'), 'Hello, Wallace Russel Osbourne! Welcome to Albany, New York!')
         test.assert_equals(say_hello(['Lupin','the','Third'],'Los Angeles','California'), 'Hello, Lupin the Third! Welcome to Los Angeles, California!')
         test.assert_equals(say_hello(['Marlo','Stanfield'],'Baltimore','Maryland'), 'Hello, Marlo Stanfield! Welcome to Baltimore, Maryland!')
+
+#  № 2 Jenny has written a function that returns a greeting for a user.
+#  However, she's in love with Johnny, and would like to greet him slightly different.
+#  She added a special case to her function, but she made a mistake.
+#
+# Can you help her?
 
 
