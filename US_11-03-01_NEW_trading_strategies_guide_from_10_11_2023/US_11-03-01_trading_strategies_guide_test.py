ae816eba654761cdc58d8b190ce7098d1f8fe5f4
Mila Podchasova <Pod4asova20@gmail.com>
2024-01-22 14:25:35 +0300

    Update US_11-03-01_trading_strategies_guide_test.py

--- US_11-03-01_NEW_trading_strategies_guide_from_10_11_2023/US_11-03-01_trading_strategies_guide_test.py
+++ US_11-03-01_NEW_trading_strategies_guide_from_10_11_2023/US_11-03-01_trading_strategies_guide_test.py
@@ -4,6 +4,7 @@ import pytest
 from datetime import datetime
 import random  # for new method
 
+
 from conf import QTY_LINKS
 from pages.Elements.ButtonStartTradingMainBanner import MainBannerStartTrading
 from pages.Elements.ButtonTradeOnWidgetMostTraded import ButtonTradeOnWidgetMostTraded
