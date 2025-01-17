c3ab85b962ea26a35522984b1ae5a0a620354822
Mila Podchasova <Pod4asova20@gmail.com>
2025-01-17 17:36:06 +0300

    Create Links.py

---
+++ Links.py
@@ -0,0 +1,4 @@
+LINK_PAGINATIONS_11_LOCATOR = (By.CSS_SELECTOR, " #alc > div.cardsNews_pagination__1p_3X > div > nav > a:nth-child(5)")
+LINK_PAGE_BP_SHARE_LOCATOR = (By.XPATH, "//*[@id='alc']/div[1]/div[8]/div/a/b[contains(text(), 'BP share price')]")
+LINK_RESEARCH_TEAM_LOCATOR = (By.XPATH, "//a[contains(text(), 'Research Team')]")
+PAGE_RESEARCH_TEAM_LOCATOR = (By.XPATH, "//h1[contains(text(), 'Research Team')]")
